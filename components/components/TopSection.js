bc484b615fc46f8f5d16fcbfa9bf7fbc363479af
Apollo Zeus Espinosa <96609384+apollozeus042@users.noreply.github.com>
2024-04-21 20:06:40 +0800

    visual change

--- components/components/TopSection.js
+++ components/components/TopSection.js
@@ -29,7 +29,7 @@ const LeftBox = ({
   return (
     <>
       <Box
-        w={["40em", "40em", "40em", "40em", "40em", "70em"]}
+        w={["40em", "40em", "40em", "40em", "40em", "40em"]}
         mr={[0, 0, 0, 0, 0, 24]}
       >
         <Flex align="Aflex-end" justify="space-between">
