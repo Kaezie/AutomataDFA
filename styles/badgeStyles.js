3f6b136bde8057f84b3b418f5ea12fe4d9ad6824
Apollo Zeus Espinosa <96609384+apollozeus042@users.noreply.github.com>
2024-04-21 21:22:16 +0800

    change node outline

--- styles/badgeStyles.js
+++ styles/badgeStyles.js
@@ -35,115 +35,115 @@ export const Badge = {
       top: "50%",
       left: "10%",
 
-      borderColor: "blue.300",
+      borderColor: "green.300",
     },
     q2: {
       top: "20%",
       left: "20%",
 
-      borderColor: "orange.300",
+      borderColor: "blue.300",
     },
     q3: {
       top: "20%",
       left: "40%",
 
-      borderColor: "purple.300",
+      borderColor: "blue.300",
     },
     q4: {
       top: "80%",
       left: "20%",
 
-      borderColor: "orange.300",
+      borderColor: "blue.300",
     },
     q5: {
       top: "80%",
       left: "40%",
 
-      borderColor: "purple.300",
+      borderColor: "blue.300",
     },
     q6: {
       top: "50%",
       left: "50%",
 
-      borderColor: "orange.300",
+      borderColor: "blue.300",
     },
     q7: {
       top: "20%",
       left: "60%",
 
-      borderColor: "orange.300",
+      borderColor: "blue.300",
     },
     q8: {
       top: "80%",
       left: "60%",
 
-      borderColor: "orange.300",
+      borderColor: "blue.300",
     },
     q9: {
       top: "50%",
       left: "70%",
 
-      borderColor: "purple.300",
+      borderColor: "blue.300",
     },
     q10: {
       top: "50%",
       left: "90%",
 
-      borderColor: "teal.300",
+      borderColor: "green.300",
     },
     T: {
       top: "50%",
       left: "30%",
 
-      borderColor: "pink.300",
+      borderColor: "red.300",
     },
     1: {
       top: "15%",
       left: "70%",
 
-      borderColor: "orange.300",
+      borderColor: "blue.300",
     },
     2: {
       top: "50%",
       left: "70%",
 
-      borderColor: "purple.300",
+      borderColor: "blue.300",
     },
     3: {
       top: "85%",
       left: "70%",
 
-      borderColor: "orange.300",
+      borderColor: "blue.300",
     },
     4: {
       top: "50%",
       left: "50%",
 
-      borderColor: "orange.300",
+      borderColor: "blue.300",
     },
     5: {
       top: "50%",
       left: "90%",
 
-      borderColor: "teal.300",
+      borderColor: "green.300",
     },
     6: {
       top: "85%",
       left: "50%",
 
-      borderColor: "purple.300",
+      borderColor: "blue.300",
     },
     7: {
       top: "50%",
       left: "30%",
 
-      borderColor: "purple.300",
+      borderColor: "blue.300",
     },
     8: {
       top: "50%",
       left: "10%",
 
-      borderColor: "blue.300",
+      borderColor: "green.300",
     },
   },
   defaultProps: {
