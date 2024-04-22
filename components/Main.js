39cd74c83d834e1f6e0823c5124a0f34e69d8ffe
Apollo Zeus Espinosa <96609384+apollozeus042@users.noreply.github.com>
2024-04-23 03:48:01 +0800

    simulation fix

--- components/Main.js
+++ components/Main.js
@@ -149,19 +149,20 @@ const Main = () => {
         results = new DFA(input, problem1, language1);
         const pathWithZeroes = [0].concat(...results.path.map((e) => [e, 0]));
         let isValid = false; // Track if a valid path has been found
-        pathWithZeroes.forEach((node, i) => {
+        pathWithZeroes.some((node, i) => {
           setTimeout(() => {
-            setCurrentNode(node);
-            if (!isValid && node === pathWithZeroes[pathWithZeroes.length - 2] && !pathWithZeroes.includes("T") && !pathWithZeroes.includes("eos") && i === pathWithZeroes.length - 2) {
-              handleValid();
-              isValid = true;
-            } else if (node === "T" && pathWithZeroes.slice(-4)[0] === "T") {
-              handleTrapped();
-            } else if (pathWithZeroes.slice(-4)[3 - 1] === node && !pathWithZeroes.includes("T") && i === pathWithZeroes.length - 2) {
-              handleShort();
-            }
+              setCurrentNode(node);
+              node == pathWithZeroes[pathWithZeroes.length - 2] &&
+              !pathWithZeroes.includes("T") &&
+              !pathWithZeroes.includes("eos") && i === pathWithZeroes.length - 2
+                  ? handleValid()
+                  : node == "T" && pathWithZeroes.slice(-4)[0] == "T"
+                  ? handleTrapped()
+                  : pathWithZeroes.slice(-4)[3 - 1] == node &&
+                  !pathWithZeroes.includes("T") && i === pathWithZeroes.length - 2 && 
+                  handleShort();
           }, i * 200);
-        });
+      });
       } else {
         notInLanguageToast();
       }
