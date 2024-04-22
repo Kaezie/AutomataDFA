b4aa120ce323ab87756356160d493cb09089f3f0
Apollo Zeus Espinosa <96609384+apollozeus042@users.noreply.github.com>
2024-04-23 03:53:51 +0800

    simulating fix

--- components/Main.js
+++ components/Main.js
@@ -159,7 +159,7 @@ const Main = () => {
                   : node == "T" && pathWithZeroes.slice(-4)[0] == "T"
                   ? handleTrapped()
                   : pathWithZeroes.slice(-4)[3 - 1] == node &&
-                  !pathWithZeroes.includes("T")
+                  !pathWithZeroes.includes("T") && i === pathWithZeroes.length - 2 && 
                   handleShort();
           }, i * 200);
       });
