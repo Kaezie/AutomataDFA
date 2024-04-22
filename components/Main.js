d479e931b53d5fcbd1950c2b26c0fb183e60a753
Apollo Zeus Espinosa <96609384+apollozeus042@users.noreply.github.com>
2024-04-23 03:41:04 +0800

    simulating trap revert

--- components/Main.js
+++ components/Main.js
@@ -155,7 +155,7 @@ const Main = () => {
             if (!isValid && node === pathWithZeroes[pathWithZeroes.length - 2] && !pathWithZeroes.includes("T") && !pathWithZeroes.includes("eos") && i === pathWithZeroes.length - 2) {
               handleValid();
               isValid = true;
-            } else if (node === "T" && pathWithZeroes.slice(-4)[0] === "T" && i === pathWithZeroes.length - 2) {
+            } else if (node === "T" && pathWithZeroes.slice(-4)[0] === "T") {
               handleTrapped();
             } else if (pathWithZeroes.slice(-4)[3 - 1] === node && !pathWithZeroes.includes("T") && i === pathWithZeroes.length - 2) {
               handleShort();
