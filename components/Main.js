04fbfbe3430ae63e25e82e935479becf6e1ccf4e
Apollo Zeus Espinosa <96609384+apollozeus042@users.noreply.github.com>
2024-04-23 05:04:46 +0800

    logic fix

--- components/Main.js
+++ components/Main.js
@@ -150,7 +150,7 @@ const Main = () => {
             if (!isValid && node === pathWithZeroes[pathWithZeroes.length - 2] && !pathWithZeroes.includes("T") && !pathWithZeroes.includes("eos")) {
               handleValid();
               isValid = true;
-            } else if (node == "T" && pathWithZeroes.slice(-4)[0] === "T") {
+            } else if (node == "T" && pathWithZeroes.slice(-4)[0] == "T") {
               handleTrapped();
             } else if (pathWithZeroes.slice(-4)[3 - 1] == node && !pathWithZeroes.includes("T")) {
               handleShort();
