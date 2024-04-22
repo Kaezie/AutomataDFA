1ce1df577ae2bba92fb229aba4a20256de907507
Apollo Zeus Espinosa <96609384+apollozeus042@users.noreply.github.com>
2024-04-23 05:01:36 +0800

    logic fix

--- components/Main.js
+++ components/Main.js
@@ -144,20 +144,19 @@ const Main = () => {
         results = new DFA(input, problem1, language1);
         const pathWithZeroes = [0].concat(...results.path.map((e) => [e, 0]));
         let isValid = false; // Track if a valid path has been found
-        pathWithZeroes.some((node, i) => {
+        pathWithZeroes.forEach((node, i) => {
           setTimeout(() => {
-              setCurrentNode(node);
-              node == pathWithZeroes[pathWithZeroes.length - 2] &&
-              !pathWithZeroes.includes("T") &&
-              !pathWithZeroes.includes("eos") && i === pathWithZeroes.length - 2
-                  ? handleValid()
-                  : node == "T" && pathWithZeroes.slice(-4)[0] == "T"
-                  ? handleTrapped()
-                  : pathWithZeroes.slice(-4)[3 - 1] == node &&
-                  !pathWithZeroes.includes("T") && i === pathWithZeroes.length - 2 && 
-                  handleShort();
+            setCurrentNode(node);
+            if (!isValid && node === pathWithZeroes[pathWithZeroes.length - 2] && !pathWithZeroes.includes("T") && !pathWithZeroes.includes("eos")) {
+              handleValid();
+              isValid = true;
+            } else if (node == "T" && pathWithZeroes.slice(-4)[0] === "T") {
+              handleTrapped();
+            } else if (pathWithZeroes.slice(-4)[3 - 1] == node && !pathWithZeroes.includes("T")) {
+              handleShort();
+            }
           }, i * 200);
-      });
+        });
       } else {
         notInLanguageToast();
       }
