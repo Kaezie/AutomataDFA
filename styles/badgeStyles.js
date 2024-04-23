3a1451cb62173fe5db0170c1adc1319e5f43d5b9
Apollo Zeus Espinosa <96609384+apollozeus042@users.noreply.github.com>
2024-04-23 12:20:01 +0800

    badge fix

--- styles/badgeStyles.js
+++ styles/badgeStyles.js
@@ -92,9 +92,7 @@ export const Badge = {
       borderColor: "green.300",
     },
     q10inner: {
-      top: "50%",
-      left: "90%",
-      size:"70%",
+      size:"40%",
       borderColor: "green.300",
     },
     T: {
