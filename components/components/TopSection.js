8600c0bcd5ddf34d0b152063f46bb4f7499616ba
Apollo Zeus Espinosa <96609384+apollozeus042@users.noreply.github.com>
2024-04-23 11:41:23 +0800

    change box valid check

--- components/components/TopSection.js
+++ components/components/TopSection.js
@@ -72,17 +72,17 @@ const LeftBox = ({
                     variant="data"
                     rightIcon={
                       data.result == "Valid" ? (
-                        <Box background-color="teal.300">
+                        <Box bg='teal.300'>
                         </Box>
                       ) : (
                         data.result == "Invalid" && (
-                          <Box background-color="pink.300">
+                          <Box bg='pink.300'>
                           </Box>
                         )
                       )
                     }
                   >
-                    {data.result}
+                    {data.result} wow
                   </Button>
                 )}
               </Flex>
