760e8fdbb093a6953fc570ba29092de9553f8c26
Apollo Zeus Espinosa <96609384+apollozeus042@users.noreply.github.com>
2024-05-15 22:44:24 +0800

    change popoverplacement

--- components/components/PDA.js
+++ components/components/PDA.js
@@ -1,53 +1,55 @@
+import React from "react";
 import {
-    Box,
-    VStack,
-    Text,
-    Image,
-    Button,
-    Popover,
-    PopoverTrigger,
-    PopoverContent,
-    PopoverHeader,
-    PopoverBody,
-    PopoverArrow,
-    PopoverCloseButton,
-  } from "@chakra-ui/react";
+  Box,
+  VStack,
+  Text,
+  Image,
+  Button,
+  Popover,
+  PopoverTrigger,
+  PopoverContent,
+  PopoverHeader,
+  PopoverBody,
+  PopoverArrow,
+  PopoverCloseButton,
+  useDisclosure,
+} from "@chakra-ui/react";
 
-  const PDA = ({ prob2 }) => {
-    return (
-      <>
-        <Popover isLazy placement="top">
-          <PopoverTrigger>
-            <Button mx="2">Show PDA</Button>
-          </PopoverTrigger>
-          <PopoverContent
-            _focus=""
-            bg="gray.800"
-            fontSize={["0.7em", "0.7em", "0.9em"]}
-          >
-            <PopoverArrow />
-            <PopoverCloseButton />
-            <PopoverHeader fontWeight="semibold">
-            Push Down Automata
-            </PopoverHeader>
-            <PopoverBody align="center">
-              {!prob2 ? (
-                <Image
-                  size="1920px"
-                  src="/PDA_AB.png"
-            />
-              ) : (
-                <Image 
-                  size="1920px"
-                  src="/PDA_01.png"
-                />
-              )}
-            </PopoverBody>
-          </PopoverContent>
-        </Popover>
-      </>
-    );
-  };
+const PDA = ({ prob2 }) => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
   
-  export default PDA;
-  
+  return (
+    <>
+      <Button onClick={onOpen} mx="2">
+        Show PDA
+      </Button>
+      <Popover
+        isOpen={isOpen}
+        onClose={onClose}
+        placement="top"
+        closeOnBlur={false}
+      >
+        <PopoverContent
+          _focus=""
+          bg="gray.800"
+          fontSize={["0.7em", "0.7em", "0.9em"]}
+          width="80%" // Adjust width as needed
+          maxW="500px" // Adjust max width as needed
+        >
+          <PopoverArrow />
+          <PopoverCloseButton />
+          <PopoverHeader fontWeight="semibold">Push Down Automata</PopoverHeader>
+          <PopoverBody align="center">
+            {!prob2 ? (
+              <Image size="1920px" src="/PDA_AB.png" />
+            ) : (
+              <Image size="1920px" src="/PDA_01.png" />
+            )}
+          </PopoverBody>
+        </PopoverContent>
+      </Popover>
+    </>
+  );
+};
+
+export default PDA;
